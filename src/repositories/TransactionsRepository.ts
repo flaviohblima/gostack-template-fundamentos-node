84ce3ea2a8e9bd899667792956cef32cf1b04749
Flavio Lima <flavio.hblima@gmail.com>
2021-08-07 20:24:58 -0300

    FEAT: Transaction: create; getBalance; Err when too expensive - prettier prefferences

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,63 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
+}
+
+interface CreateTransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (balaceMap, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            return {
+              ...balaceMap,
+              income: balaceMap.income + transaction.value,
+              total: balaceMap.total + transaction.value,
+            }
+          case 'outcome':
+            return {
+              ...balaceMap,
+              outcome: balaceMap.outcome + transaction.value,
+              total: balaceMap.total - transaction.value,
+            }
+          default:
+            return balaceMap
+        }
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,30 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
+
+interface Request {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
 
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+  private transactionsRepository: TransactionsRepository
 
   constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
+    this.transactionsRepository = transactionsRepository
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance()
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new Error(
+        'Transaction not allowed. The value onverflows your total balance.',
+      )
+    }
+
+    return this.transactionsRepository.create({ title, value, type })
   }
 }
 
-export default CreateTransactionService;
+export default CreateTransactionService
